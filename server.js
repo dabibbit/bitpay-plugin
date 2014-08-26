8c9783b4ca0cbae571843e82aae5855f1ef01368
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-26 08:34:11 -0700

    [FIX] Specify extended option for urlencoded.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/bitpay/callbacks', function(request, response){ 
   console.log(request.body);
